0aa3d6c989f20ad3dd72bfb65cc6be0ca81ee526
meetshah15 <meetshah15@gmail.com>
2020-10-15 00:57:42 +0530

    Removed token

--- discord.py
+++ discord.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 
-token = 'NzY1NjI2OTU2OTY3ODM3NzE3.X4Xjqw.2LAWE9k1tKaF_KKPJEqYal6dylU';
 client = commands.Bot(command_prefix = '.')
 
 @client.event
