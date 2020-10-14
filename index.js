4efa8b7cacb3b5f2f5d53b300858e0e61139a6e2
meetshah15 <meetshah15@gmail.com>
2020-10-15 00:56:05 +0530

    Taking token from variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 
-const token = 'NzY1NjI2OTU2OTY3ODM3NzE3.X4Xjqw.A3CDUp_O2eX73huXUe2Ouktm5dE';
+const token = process.env.token;
 
 bot.on('ready', () => {
     console.log('Bot is online');
